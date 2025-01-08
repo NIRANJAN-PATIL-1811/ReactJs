e33a2f91c373418d2144b7e683f95f35d816ae22
NIRANJAN-PATIL-1811 <rosh.rp1811@gmail.com>
2025-01-08 15:38:05 +0530

    Add new React Js Program

---
+++ practice/src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+
+function App(){
+  return (
+    <>
+      <div className="main-div">
+        <div className="sub1">
+
+          {/* <div className="input-div">
+            <input className="input-div1" type="text" />
+          </div> */}
+
+          <div className="btn">
+            <button className="btn1" >Add</button>
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
